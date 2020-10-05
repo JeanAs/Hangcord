6c723d10ca21eab6a07bad895073db2aa22086cf
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-10-05 21:54:14 +0530

    Make sure to add good types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,17 @@
+class HangmanGame{
+  gameEmbed: any;
+  inGame: boolean;
+  word: string;
+  guessed: any[];
+  wrongs: number;
+  
+  public constructor();
+  
+  public newGame(msg: string, options: optionType): void;
+  public makeGuess(reaction: string): any;
+  public gameOver(win: any): any;
+  public getDescription(): string;
+  public waitForReaction(): any;
+}
 
+export default HangmanGame
