d1981682e5c6b00913767d4bf5b1d521d44942ac
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-10-05 22:02:37 +0530

    Fixed typings

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,19 @@
+interface optionType{
+  title?: string,
+  color?: string
+}
+
 class HangmanGame{
   gameEmbed: any;
   inGame: boolean;
   word: string;
   guessed: any[];
   wrongs: number;
+  options: optionType;
   
-  public constructor();
+  public constructor(options: optionType);
   
-  public newGame(msg: string, options: optionType): void;
+  public newGame(msg: string): void;
   public makeGuess(reaction: string): any;
   public gameOver(win: any): any;
   public getDescription(): string;
